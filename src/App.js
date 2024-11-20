efd0d17f8e59e51850a1c3a87259cb1b583b3637
Luiz Filipe Guerino <lfguerino@gmail.com>
2024-11-20 13:55:08 -0300

    Adicionando Componente de Posts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,35 @@
 import React from 'react';
 
+import Post from './Post';
+
 function App() {
   return (
     <>
-      <h1>Componente App!</h1>
-      <h2>Subtitulo</h2>
+      <h1>LFGuerino Blog</h1>
+      <h2>Posts da Semana</h2>
+      <hr />
+
+      <Post
+        post={{
+          title: 'Titulo 01',
+          subtitle: 'Subtitulo 01'
+        }}
+      />
+
+      <Post
+        post={{
+          title: 'Titulo 02',
+          subtitle: 'Subtitulo 02'
+        }}
+      />
+
+      <Post
+        post={{
+          title: 'Titulo 03',
+          subtitle: 'Subtitulo 03'
+        }}
+      />
+
     </>
   );
 }
---
+++ src/Post.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function Post(props) {
+  return (
+    <>
+      <article>
+        <strong>{props.post.title}</strong><br />
+        <small>{props.post.subtitle}</small>
+      </article>
+      <br />
+    </>
+  );
+
+}
+export default Post;
